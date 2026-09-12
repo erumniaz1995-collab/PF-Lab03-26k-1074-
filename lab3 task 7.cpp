a6010ae81815444bb28bdb2b1e329abf904d1a15
erumniaz1995-collab <erumniaz1995@gmail.com>
2026-09-13 02:30:08 +0500

    Add files via upload
    i added the task 6 and 7

---
+++ lab3 task 7.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int age;
+	float height;
+	printf("enter your age and height :");
+	scanf("%d %f",&age ,&height);
+	printf("you are %d years old and %.2f meter tall",age,height);
+		return 0;
+}
